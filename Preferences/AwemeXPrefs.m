06901bb4c46afe94ba38200b1c697b02b5ac25ca
kulinichx <141639436+kulinichx@users.noreply.github.com>
2026-06-13 19:57:52 +0800

    Update AwemeXPrefs.m

--- Preferences/AwemeXPrefs.m
+++ Preferences/AwemeXPrefs.m
@@ -2,10 +2,12 @@
 #import <Preferences/PSSpecifier.h>
 
 @implementation AwemeXPrefsListController
+
 - (NSArray *)specifiers {
     if (!_specifiers) {
-        _specifiers = [[self loadSpecifiersFromPlistName:@"Root" target:self] retain];
+        _specifiers = [self loadSpecifiersFromPlistName:@"Root" target:self];
     }
     return _specifiers;
 }
+
 @end
